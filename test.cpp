b9691fd6c303901e676de7382383518a7b8f51d3
Alpha-Project <23101024@uap-bd.edu>
2024-01-28 02:17:37 +0600

    New

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define nl "\n"
+#define blk " "
+typedef long long ll;
+// #define ll long long
+using namespace std;
+const int N=1e5+5;
+int main()
+{
+    cout<<"Hello";
+}
